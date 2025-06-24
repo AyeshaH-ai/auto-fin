0056231ba168554c6fc9af97e8a216ae5c2afb36
Ayesha Hussain <corevestfinance1@gmail.com>
2025-06-24 17:17:56 +0500

    Update visuals.py

--- visuals.py
+++ visuals.py
@@ -2,9 +2,11 @@ import streamlit as st
 import plotly.express as px
 
 def generate_visuals(df):
-    fig = px.bar(df.groupby('Description')['Amount'].sum().reset_index(),
-                 x='Description', y='Amount', title='Spending by Category')
-    st.plotly_chart(fig)
+    # Total by Category
+    cat_chart = px.bar(df.groupby('Category')['Amount'].sum().reset_index(),
+                       x='Category', y='Amount', title='Total by Category')
+    st.plotly_chart(cat_chart)
 
-    fig2 = px.pie(df, names='Description', values='Amount', title='Category Distribution')
-    st.plotly_chart(fig2)
+    # Pie chart by Payment Method
+    payment_chart = px.pie(df, names='Payment Method', values='Amount', title='Payments Breakdown')
+    st.plotly_chart(payment_chart)
